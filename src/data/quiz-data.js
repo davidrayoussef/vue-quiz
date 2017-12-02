dcff9656879a41a21556f5285fe59cf6ff2553cf
David Ra Youssef <davidradeveloper@gmail.com>
2017-12-02 14:27:11 -0500

    Add question data

--- src/data/quiz-data.js
+++ src/data/quiz-data.js
@@ -1,4 +1,4 @@
-export const questions = [
+const QUESTIONS = [
   {
     "question":"What's the difference between grid and flexbox?",
     "answers": [
@@ -29,82 +29,122 @@ export const questions = [
     ],
     "correct": 1
   },
+  {
+    "question": "What is the order of specificity from greatest to least?",
+    "answers": [
+      "#id > !important > .class > tag",
+      "#id > .class > tag > !important",
+      "!important > #id > .class > tag",
+      "None of the above."
+    ],
+    "correct": 2
+  },
+  {
+    "question": "Why would you use the z-index property?",
+    "answers": [
+      "To move an element from the right to the left.",
+      "To move an overlapped element from the back to the front.",
+      "To move an element from the top of the page to the bottom of the page.",
+      "To change an element's width and height."
+    ],
+    "correct": 1
+  },
+  {
+    "question": "What's the order of values of single-declarations for the margin and padding properties, e.g. margin: 20px 10px 30px 40px.",
+    "answers": [
+      "Left Right Top Bottom",
+      "Top Left Right Bottom",
+      "Top Right Bottom Left",
+      "None of the above."
+    ],
+    "correct": 2
+  },
+  {
+    "question": "What are the four properties of the Box Model?",
+    "answers": [
+      "Width, height, padding, margins",
+      "Horizontal margins, vertical margins, horizontal padding, vertical padding",
+      "Content (width and height), padding, borders, margins",
+      "None of the above."
+    ],
+    "correct": 2
+  },
+  {
+    "question": "Which of the following is TRUE about the Box Model?",
+    "answers": [
+      "Padding specifies the space between the content area and the border.",
+      "Border specifies the area between the padding on the inside and margin on the outside.",
+      "Margin specifies the space outside the border.",
+      "All of the above."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Which of the following will target a paragraph tag inside a div ONLY if it's an immediate child?",
+    "answers": [
+      "div p",
+      "div > p",
+      "div + p",
+      "div ~ p"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "Which of the following is true about the box-sizing property?",
+    "answers": [
+      "box-sizing: border-box includes border thickness and padding in the overall dimensions of the box; prevents border and padding from affecting the size.",
+      "box-sizing: border-box automatically adjusts the content area based on the remainding width, after subtracting border and padding.",
+      "The default is box-sizing: content-box, which ?????",
+      "All of the above. // Test this out in the browser"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Which is true about the difference between position: relative and position: absolute?",
+    "answers": [
+      "position: relative positions an element relative to its default static position (relative to where it normally would have been).",
+      "An element set to absolute will be ignored by other elements on the page?",
+      "position: absolute positions an element relative to the last positioned parent element, or body if there are none.",
+      "All of the above."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "What's the difference between an ID and a class?",
+    "answers": [
+      "An ID is unique and is used on one element; a class can be used on many elements.",
+      "IDs have higher specificity than classes.",
+      "More than one class can be used for an element.",
+      "All of the above."
+    ],
+    "correct": 3
+  }
 ];
 
+//
+// What's the difference between display: none and visibility: hidden?
+// An element with display: none will be completely removed from the web page.
+// An element with visibility: hidden will not be visible on the web page, but the space occupied by it will.
+// They both remove an element from the DOM.
+//
+//
+// Which properties are used to affect the positioning of elements in the browser?
+// position display z-index float and clear
+// something something transition something
+// something something visibility
+// Only JavaScript allows you to affect positioning.
+//
+// What are the four position properties for block? elements?
+// static relative absolute and fixed
+//
+// What's an instance where you would use the calc() function
+//
+// Which of the following is an example of the BEM pattern?
+//
+// performance in animations ... transform vs ?
+//
+// How do you center a div within a div in JavaScript?
+// margin: 0 auto; in the containing elements
+// display: flex; align-items center; on the parent element
 
-Which of the following is an example of the BEM pattern?
-
-performance in animations ... transform vs ?
-
-How do you center a div within a div in JavaScript?
-margin: 0 auto; in the containing elements
-display: flex; align-items center; on the parent element
-
-specificity order in css: !important > #id > .class > tag
-
-22. Differentiate Class selector from ID selector?
-While an overall block is given to class selector, ID selector prefers only a single element differing from other elements. In other words, ID are uniques while classes are not. Its possible that an element has both class and ID.
-
-25. How to overrule underlining Hyperlinks?
-text-decoration: none
-
-36. How does Z index function?
-Overlapping may occur while using CSS for positioning HTML elements. Z index helps in specifying the overlapping element. It is a number which can be positive or negative, the default value being zero.
-
-What are the four properties of the Box Model?
-Content height and width, padding, borders, and margins
-
-Which of the following is TRUE about the Box Model?
-Padding specifies the space between the content area and the border.
-Border specifies the area between the padding on the inside and margin on the outside.
-Margin specifies the space outside the border.
-All of the above
-
-WHich of the following will target a p inside a div????
-div, p
-div p
-div > p
-div + p
-div ~ p
-
-
-What's an instance where you would use the calc() function
-
-What's the order of values of single-declarations for the margin and padding properties, e.g. margin: 20px 10px 30px 40px.
-Left Right Top Bottom
-Top Left Right Bottom
-Top Right Bottom Left
-None of the Above.
-
-
-What's the difference between display: none and visibility: hidden?
-An element with display: none will be completely removed from the web page.
-An element with visibility: hidden, however, will not be visible on the web page, but the space reserved for it will.
-Using visiblility: hidden rather than display: none will prevent redraws ???
-
-
-Which of the following is true about the declaration box-sizing: border-box do?
-Includes border thickness and padding in the overall dimensions of the box; prevents border and padding from affecting the size.
-Makes the width property the same as the actual rendered width of the element.
-Automatically adjusts the content area based on the remainding width, after subtracting border and padding.
-All of the above.
-// Test this out in the browser
-
-Which properties are used to affect the positioning of elements in the browser?
-position display z-index float and clear
-something something transition something
-something something visibility
-Only JavaScript allows you to affect positioning.
-
-What are the four position properties for block? elements?
-static relative absolute and fixed
-
-Which is true about the difference between position: relative and position: absolute?
-relative positions an element relative to its default static position (relative to where it normally would have been).
-An element set to absolute will be ignored by other elements on the page?
-absolute positions an element relative to the last positioned parent element, or body if there are none.
-
-What's the difference between an ID and a class?
-An ID is unique and is used on one element; a class can be used on many elements.
-IDs have higher specificity than classes.
-More than one class can be used for an element.
+export default QUESTIONS;
