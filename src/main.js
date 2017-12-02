f76a9500c938c62c41ab8e5c7427e38c3fd1372a
David Ra Youssef <davidradeveloper@gmail.com>
2017-12-02 14:29:25 -0500

    Add main.js

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import QUESTIONS from './data/quiz-data';
+const { shuffle } = require('funcifyr');
+
+shuffle(QUESTIONS);
+
+new Vue({
+  el: '#app',
+  data: {
+    questions: QUESTIONS,
+    step: 0,
+    userResponses: Array(QUESTIONS.length).fill(false)
+  },
+  methods: {
+    next() {
+      this.step++;
+    },
+    getScore() {
+      return this.userResponses.filter(v => v === true).length;
+    }
+  }
+});
