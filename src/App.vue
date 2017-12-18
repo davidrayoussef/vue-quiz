3088832d886df81837c42eb603b8079671bb7f6d
David Ra Youssef <davidradeveloper@gmail.com>
2017-12-18 11:14:19 -0500

    Add code tags and styles

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,34 @@
   <div id="app">
     <header>
       <h1>CSS Quiz</h1>
-      <p v-show="step < questions.length">Question <span>{{ ~~step + 1 }}</span> of <span>{{ questions.length }}</span></p>
+      <p v-if="step < questions.length">Question <span>{{ ~~step + 1 }}</span> of <span>{{ questions.length }}</span></p>
     </header>
-    <section :id="'s' + qIndex" v-for="(question, qIndex) in questions" v-if="qIndex === step">
-      <h2><p>{{ question.question }}</p></h2>
+    <section
+      :id="'s' + qIndex"
+      v-for="(question, qIndex) in questions"
+      v-if="qIndex === step">
+      <h2>
+        <p class="question" v-html="question.question"></p>
+      </h2>
       <ol>
         <li v-for="(answer, aIndex) in question.answers" :class="{ 'mark-correct': question.correct === aIndex && qIndex === step }">
-            <input
-              type="radio"
-              :id="'i' + qIndex + aIndex"
-              :value="question.correct === aIndex ? true : aIndex"
-              :name="qIndex"
-              v-model="userResponses[qIndex]">
-          <label :for="'i' + qIndex + aIndex">{{ answer }}</label>
+          <input
+            type="radio"
+            :id="'i' + qIndex + aIndex"
+            :value="question.correct === aIndex ? true : aIndex"
+            :name="qIndex"
+            v-model="userResponses[qIndex]">
+          <label :for="'i' + qIndex + aIndex" v-html="answer"></label>
         </li>
       </ol>
     </section>
     <section class="results" v-for="(result, rIndex) in questions" v-if="rIndex + .5 === step">
-      <h2><p>{{ result.question }}</p></h2>
+      <h2><p class="question" v-html="result.question"></p></h2>
       <ol>
         <li
           v-for="(answer, aIndex) in result.answers"
           :class="{ visible: result.answers[result.correct] === 'All of the above.' ? true : result.correct === aIndex }">
-          <p>{{ answer }}</p>
+          <p v-html="answer"></p>
         </li>
       </ol>
     </section>
--- src/data/quiz-data.js
+++ src/data/quiz-data.js
@@ -1,10 +1,10 @@
 const QUESTIONS = [
   {
-    "question":"What's the difference between grid and flexbox?",
+    "question":"What's the difference between <code>grid</code> and <code>flexbox</code>?",
     "answers": [
-      "Flexbox is used for laying out items in a single dimension; a row OR a column.",
-      "Grid is for the layout of items in two dimensions; rows AND columns.",
-      "Flexbox is better for positioning elements within a block, while grid is better for laying out the main components of a page.",
+      "<code>flexbox</code> is used for laying out items in a single dimension; a row OR a column.",
+      "<code>grid</code> is for the layout of items in two dimensions; rows AND columns.",
+      "<code>flexbox</code> is better for positioning elements within a block, while <code>grid</code> is better for laying out the main components of a page.",
       "All of the above."
     ],
     "correct": 3
@@ -14,7 +14,7 @@ const QUESTIONS = [
     "answers": [
       "Media queries.",
       "Flexible units like percent, vw, vh, em, rem.",
-      "CSS features like grid and flexbox.",
+      "CSS features like <code>grid</code> and <code>flexbox</code>.",
       "All of the above."
     ],
     "correct": 3
@@ -30,17 +30,17 @@ const QUESTIONS = [
     "correct": 1
   },
   {
-    "question": "What is the order of specificity from highest to least?",
+    "question": "What's the order of specificity from highest to least?",
     "answers": [
-      "#id > !important > .class > tag",
-      "#id > .class > tag > !important",
-      "!important > #id > .class > tag",
+      "<code>#id</code> > <code>!important</code> > <code>.class</code> > <code>tag</code>",
+      "<code>#id</code> > <code>.class</code> > <code>tag</code> > <code>!important</code>",
+      "<code>!important</code> > <code>#id</code> > <code>.class</code> > <code>tag</code>",
       "None of the above."
     ],
     "correct": 2
   },
   {
-    "question": "Why would you use the z-index property?",
+    "question": "Why would you use the <code>z-index</code> property?",
     "answers": [
       "To move an element from the right to the left.",
       "To move an overlapped element from the back to the front.",
@@ -50,7 +50,7 @@ const QUESTIONS = [
     "correct": 1
   },
   {
-    "question": "What's the value order of single-declarations for the margin and padding properties, e.g. margin: 20px 10px 30px 40px.",
+    "question": "What's the value order of single-declarations for the margin and padding properties, e.g. <code>margin: 20px 10px 30px 40px</code>.",
     "answers": [
       "Left Right Top Bottom",
       "Top Left Right Bottom",
@@ -80,41 +80,41 @@ const QUESTIONS = [
     "correct": 3
   },
   {
-    "question": "Which of the following will target a paragraph tag inside a div ONLY if it's an immediate child?",
+    "question": "Which of the following will target a <code>p</code> tag inside a <code>div</code> ONLY if it's an immediate child?",
     "answers": [
-      "div p",
-      "div > p",
-      "div + p",
-      "div ~ p"
+      "<code>div p</code>",
+      "<code>div > p</code>",
+      "<code>div + p</code>",
+      "<code>div ~ p</code>"
     ],
     "correct": 1
   },
   {
-    "question": "Which of the following is true about the box-sizing property?",
+    "question": "Which of the following is true about the <code>box-sizing</code> property?",
     "answers": [
-      "box-sizing: border-box includes border thickness and padding in the overall dimensions of the box; it prevents border and padding from affecting the size.",
-      "box-sizing: border-box automatically adjusts the content area based on the remaining width, after subtracting border and padding.",
-      "The default is box-sizing: content-box, which ?????",
+      "<code>box-sizing: border-box</code> includes border thickness and padding in the overall dimensions of the box; it prevents border and padding from affecting the size.",
+      "<code>box-sizing: border-box</code> automatically adjusts the content area based on the remaining width, after subtracting border and padding.",
+      "The default is <code>box-sizing: content-box</code>, which ?????",
       "All of the above. // Test this out in the browser"
     ],
     "correct": 3
   },
   {
-    "question": "Which is true about the difference between position: relative and position: absolute?",
+    "question": "Which is true about the difference between <code>position: relative</code> and <code>position: absolute</code>?",
     "answers": [
-      "position: relative positions an element relative to its default static position (relative to where it normally would have been).",
-      "An element set to absolute will be ignored by other elements on the page (removed from the page flow).",
-      "position: absolute positions an element relative to the last positioned parent element (or body if there are none).",
+      "<code>position: relative</code> positions an element relative to its default static position (relative to where it normally would have been).",
+      "An element set to <code>absolute</code> will be ignored by other elements on the page (removed from the page flow).",
+      "<code>position: absolute</code> positions an element relative to the last positioned parent element (or body if there are none).",
       "All of the above."
     ],
     "correct": 3
   },
   {
-    "question": "What's the difference between an ID and a class?",
+    "question": "What's the difference between an <code>id</code> and a <code>class</code>?",
     "answers": [
-      "An ID is unique and is used on one element; a class can be used on many elements.",
-      "IDs have higher specificity than classes.",
-      "More than one class can be used for an element.",
+      "An <code>id</code> is unique and is used on one element; a <code>class</code> can be used on many elements.",
+      "ids have higher specificity than classes.",
+      "More than one <code>class</code> can be used for an element.",
       "All of the above."
     ],
     "correct": 3
